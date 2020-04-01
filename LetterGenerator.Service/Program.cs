1a84c1101922a52d1b15edd76fe42c8973b602ea
namita-dias <namita.dias06@gmail.com>
2020-04-02 00:06:24 +0100

    Add the service project

---
+++ LetterGenerator.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LetterGenerator.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
